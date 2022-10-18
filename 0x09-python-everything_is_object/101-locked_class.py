369ec5a224429c7984500a81a8580991dfda4287
Zuwairanajma <juwairiyyasadiq@gmail.com>
2022-10-18 16:12:10 +0000

    Everything is objects advanced

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,13 @@
+
+cked_class.py
+
+"""Defines a locked class."""
+
+
+class LockedClass:
+    """
+    Prevent the user from instantiating new LockedClass attributes
+    for anything but attributes called 'first_name'.
+    """
+
+    __slots__ = ["first_name"]
